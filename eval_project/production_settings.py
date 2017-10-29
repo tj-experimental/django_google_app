d673a479058d0bb15978b404c8d821091c08d6c6
Tonye Jack <jtonye@ymail.com>
2017-10-29 03:57:10 -0400

    Updated staging and prod settings

--- eval_project/production_settings.py
+++ eval_project/production_settings.py
@@ -3,6 +3,8 @@ import dj_database_url
 
 DEBUG = False
 
+ALLOWED_HOSTS += ['gogglefusion.herokuapp.com']
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
--- eval_project/staging_settings.py
+++ eval_project/staging_settings.py
@@ -3,9 +3,26 @@ import dj_database_url
 
 DEBUG = False
 
+ALLOWED_HOSTS += ['gogglefusion.herokuapp.com']
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Serving static files with whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# From registration email
+REGISTRATION_DEFAULT_FROM_EMAIL = 'django_google@yahoo.com'
+# Set the email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Email connect to host
+EMAIL_HOST = 'smtp.mail.yahoo.com'
+# Email port
+EMAIL_PORT = 587
+# Email user to send the mail from
+EMAIL_HOST_USER = 'django_google@yahoo.com'
+# Email user password
+EMAIL_HOST_PASSWORD = 'rtlfjdckpeltxusf'
+# Enable TLS
+EMAIL_USE_TLS = True
