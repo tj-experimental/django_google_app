091439e7cfa27f821513d29b204c648a65d4ff1a
Tonye Jack <jtonye@ymail.com>
2017-10-30 11:05:23 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,15 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eval_project.settings")
+    production = os.environ.get('PRODUCTION', False)
+    staging = os.environ.get('STAGING', False)
+
+    # This should be set depending on the context.
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'eval_project.settings'
+    if production:
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'eval_project.production_settings'
+    elif staging:
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'eval_project.staging_settings'
 
     from django.core.management import execute_from_command_line
 
