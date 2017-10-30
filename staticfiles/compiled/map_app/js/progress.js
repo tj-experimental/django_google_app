1547fe8c34dc91b90c078b52d446d7cdd5b87099
Tonye Jack <jtonye@ymail.com>
2017-10-30 03:33:38 -0400

    Added compiled files

---
+++ staticfiles/compiled/map_app/js/progress.js
@@ -0,0 +1,6 @@
+"use strict";var $progress=$("#progress");var stopProgress=function stopProgress(){return $progress.addClass("done")};var errorProgress=function errorProgress(){return $progress.removeClass("success").addClass("error")};var successProgress=function successProgress(){return $progress.addClass("success").removeClass("error")};var startProgress=function startProgress(){var status=arguments.length>0&&arguments[0]!==undefined?arguments[0]:"";var duration=arguments.length>1&&arguments[1]!==undefined?arguments[1]:4000;return $({property:0}).animate({property:105},{duration:duration,step:function step(){var _percent=Math.round(this.property);$progress.removeClass("done");$progress.css("width",_percent+"%");if(_percent===105){stopProgress()}}// complete: function() {
+//     alert('complete');
+// }
+})};
+
+//# sourceMappingURL=progress.js.map
