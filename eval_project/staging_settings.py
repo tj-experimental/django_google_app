9eaeab2bbed7e46a6dfc36d4612b36b0f741b56f
Tonye Jack <jtonye@ymail.com>
2017-10-31 15:31:42 -0400

    Update staging_settings.py

--- eval_project/staging_settings.py
+++ eval_project/staging_settings.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['googlefusion.herokuapp.com']
+ALLOWED_HOSTS = ['googlefusion*.herokuapp.com']
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=600)
