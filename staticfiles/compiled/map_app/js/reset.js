ad70438ee0d359308b7fac37e587a8a4c8f259b4
Tonye Jack <tonye@coherentpath.com>
2019-04-14 19:46:49 -0400

    Fixed missing assests.

---
+++ staticfiles/compiled/map_app/js/reset.js
@@ -0,0 +1,3 @@
+"use strict";var resetAddresses=function resetAddresses(){window.startProgress();window.successProgress();$.ajax(window.resetUrl,{"method":"DELETE"}).done(function(data,textStatus,jqXHR){var message="Successfully reset addresses";renderAlerts([{message:message,level_tag:"success"}]);return true}).fail(function(jqXHR,textStatus,err){window.errorProgress();var message="Error resetting addresses";renderAlerts([{message:message,level_tag:"error"}]);console.log(textStatus);return true}).always(function(){location.reload();window.stopProgress()})};$("#reset-button").data("callable",resetAddresses);
+
+//# sourceMappingURL=reset.js.map
